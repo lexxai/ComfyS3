87e38a117489efbac521243f9af1f3a3a62c80f0
LeXXai <lexxai@gmail.com>
2025-12-31 15:56:38 +0200

    SaveVideoFilesS3 move_file: bool = True

--- src/nodes/save_video_files_s3.py
+++ src/nodes/save_video_files_s3.py
@@ -7,15 +7,18 @@ S3_INSTANCE = get_s3_instance()
 
 class SaveVideoFilesS3:
     def __init__(self):
-        self.s3_output_dir = os.getenv("S3_OUTPUT_DIR")
+        self.s3_output_dir = S3_INSTANCE.output_dir
         self.prefix_append = ""
 
     @classmethod
     def INPUT_TYPES(s):
-        return {"required": {
-            "filename_prefix": ("STRING", {"default": "VideoFiles"}),
-            "filenames": ("VHS_FILENAMES", )
-            }}
+        return {
+            "required": {
+                "filename_prefix": ("STRING", {"default": "VideoFiles"}),
+                "filenames": ("VHS_FILENAMES",),
+                "move_file": ("BOOLEAN", {"default": True}),
+            }
+        }
 
     RETURN_TYPES = ("STRING",)
     RETURN_NAMES = ("s3_video_paths",)
@@ -24,22 +27,22 @@ class SaveVideoFilesS3:
     OUTPUT_IS_LIST = (True,)
     CATEGORY = "ComfyS3"
 
-    def save_video_files(self, filenames, filename_prefix="VideoFiles"):
+    def save_video_files(self, filenames, filename_prefix: str = "VideoFiles", move_file: bool = True):
         filename_prefix += self.prefix_append
         local_files = filenames[1]
         full_output_folder, filename, counter, _, filename_prefix = S3_INSTANCE.get_save_path(filename_prefix)
         s3_video_paths = list()
-        
+
         for path in local_files:
             ext = path.split(".")[-1]
             file = f"{filename}_{counter:05}_.{ext}"
-            
+
             # Upload the local file to S3
             s3_path = os.path.join(full_output_folder, file)
-            
-            file_path = S3_INSTANCE.upload_file(path, s3_path)
-              
+
+            file_path = S3_INSTANCE.upload_file(path, s3_path, move_file=move_file)
+
             # Add the s3 path to the s3_image_paths list
             s3_video_paths.append(file_path)
-        
+
         return (s3_video_paths,)
